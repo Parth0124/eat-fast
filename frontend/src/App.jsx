7b5c935acbe497ef989a638b20ce5c055e3c86c0
Abhiraj412 <abhangparth@gmail.com>
2024-07-20 18:01:40 +0530

    added the routes for cart, order and home. now working on home page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,19 @@
+import { Route, Routes } from 'react-router-dom'
 import Navbar from '../src/components/Navbar/Navbar'
+import Home from './pages/Home/Home'
+import PlaceOrder from './pages/PlaceOrder/PlaceOrder'
+
 
 
 function App() {
   return (
     <div className="app">
       <Navbar />
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/cart' element={<Cart/>} />
+        <Route path='/order' element={<PlaceOrder/>} />
+      </Routes>
     </div>
   )
 }
---
+++ frontend/src/pages/PlaceOrder/PlaceOrder.jsx
@@ -0,0 +1,11 @@
+import './PlaceOrder.css'
+
+function PlaceOrder() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default PlaceOrder
