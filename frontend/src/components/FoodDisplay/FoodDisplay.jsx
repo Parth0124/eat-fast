d095fa7bfb4ba596218264bf0a981abbfef0958a
Abhiraj412 <abhangparth@gmail.com>
2024-07-22 11:02:08 +0530

    filtered out the menu based on the client' choice in the explore menu. if clicked on desserts, only desserts are shown

--- frontend/src/components/FoodDisplay/FoodDisplay.jsx
+++ frontend/src/components/FoodDisplay/FoodDisplay.jsx
@@ -12,7 +12,10 @@ const {food_list} = useContext(StoreContext)
         <h2>Top Dishes Near You</h2>
         <div className="food-display-list">
             {food_list.map((item, index) => {
-                return <FoodItem  key={index} id={item._id} name={item.name} description={item.description} price={item.price} image={item.image}/>
+                if(category==="All" || category === item.category)
+                {
+                  return <FoodItem  key={index} id={item._id} name={item.name} description={item.description} price={item.price} image={item.image}/>
+                }
             })}
         </div>
     </div>
