e54ec7a08acb101b0d1116b137e0c78a6cd0be9b
Abhiraj412 <abhangparth@gmail.com>
2024-07-22 10:54:03 +0530

    add to cart functionality developed. now the cart items are console logged

--- frontend/src/components/FoodItem/FoodItem.jsx
+++ frontend/src/components/FoodItem/FoodItem.jsx
@@ -1,21 +1,22 @@
-import { useState } from 'react'
+import { useContext } from 'react'
 import { assets } from '../../assets/assets'
 import './FoodItem.css'
+import { StoreContext } from '../../context/StoreContext'
 
 function FoodItem({id, name, price, description, image}) {
 
-  const [itemCount, setItemCount] = useState(0)
+  const {cartItems,addToCart,removeFromCart} = useContext(StoreContext)
 
   return (
     <div className='food-item'>
       <div className="food-item-image-container">
         <img className="food-item-image" src={image} alt="" />
-        {!itemCount 
-            ? <img className='add' onClick={() => setItemCount(prev=>prev+1)} src={assets.add_icon_white} alt='' />
+        {!cartItems[id]
+            ? <img className='add' onClick={() => addToCart(id)} src={assets.add_icon_white} alt='' />
             : <div className="food-item-counter">
-                <img onClick={() => setItemCount(prev=>prev-1)} src={assets.remove_icon_red} alt="" />
-                <p>{itemCount}</p>
-                <img onClick={() => setItemCount(prev=>prev+1)} src={assets.add_icon_green} alt="" />
+                <img onClick={() => removeFromCart(id)} src={assets.remove_icon_red} alt="" />
+                <p>{cartItems[id]}</p>
+                <img onClick={() => addToCart(id)} src={assets.add_icon_green} alt="" />
             </div>
         }
       </div>
