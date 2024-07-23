fa71889b6c55fd7bcc0f2754f85db67d7f1f3a7c
Abhiraj412 <abhangparth@gmail.com>
2024-07-23 12:34:25 +0530

    made the website responsive

--- frontend/src/components/AppDownload/AppDownload.jsx
+++ frontend/src/components/AppDownload/AppDownload.jsx
@@ -1,9 +1,14 @@
+import { assets } from '../../assets/assets'
 import './AppDownload.css'
 
 function AppDownload() {
   return (
-    <div>
-      
+    <div className='app-download' id='app-download'>
+        <p>For better experience Download <br /> Tomato App </p>
+        <div className="app-download-platforms">
+            <img src={assets.play_store} alt="" /> 
+            <img src={assets.app_store} alt="" />
+        </div>
     </div>
   )
 }
