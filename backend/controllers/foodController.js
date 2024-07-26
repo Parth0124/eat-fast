b7cb5e41db79862239842430123b63cfa6bb7afd
Abhiraj412 <abhangparth@gmail.com>
2024-07-26 19:37:00 +0530

    food item deleted

--- backend/controllers/foodController.js
+++ backend/controllers/foodController.js
@@ -34,4 +34,18 @@ const listFood = async (req,res) => {
     }
 }
 
-export {addFood, listFood}
+//remove food item
+const removeFood = async(req,res) => {
+    try {
+        const food = await foodModel.findById(req.body.id)
+        fs.unlink(`uploads/${food.image}`, () => {})
+
+        await foodModel.findByIdAndDelete(req.body.id)
+        res.json({success: true, message:"Food item deleted"})
+    } catch (error) {
+        console.log(error)
+        res.json({success: false, message:"Error"})
+    }
+}
+
+export {addFood, listFood, removeFood}
