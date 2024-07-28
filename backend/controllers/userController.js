d2428ce3699555559a4e6ec2cb70db4414cd8b17
Abhiraj412 <abhangparth@gmail.com>
2024-07-28 22:26:02 +0530

    can login user

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -10,7 +10,28 @@ const createToken = (id) => {
 
 //login user
 const loginUser = async(req,res) => {
+    const {email,password} = req.body;
+    try {
+        const user = await userModel.findOne({email});
 
+        if(!user)
+        {
+            return res.json({success:false, message:"User does not exist"});
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password)
+
+        if(!isMatch)
+        {
+            return res.json({success: false, message: "Invalid Credentials"})
+        }
+
+        const token = createToken(user._id);
+        res.json({success:true, token})
+    } catch (error) {
+        console.log(error)
+        res.json({success:false, message:"Error in logging you in!"});
+    }
 }
 
 //signup user
